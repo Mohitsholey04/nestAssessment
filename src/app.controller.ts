1336506b0eccff25c9af81960d7ee988fb305980
Mohit Sharma <130201481+Mohitsholey04@users.noreply.github.com>
2024-02-13 01:11:30 +0530

    delete the app.controller.spcs.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
-  @Get()
+  @Get('/login')
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello World! This is the login page';
   }
 }
